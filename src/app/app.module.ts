e2b73ec9aa98d7f9ed3c8cc3e003138360998be5
Kleby <kleby10@gmail.com>
2023-05-18 22:47:34 -0300

    estilização inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InputComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
